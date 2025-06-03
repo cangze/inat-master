e0970087bbaa89a6bb0a1b8c262b19fe20eee2bb
Dong BingQing <dongbq@qq.com>
2025-06-03 21:57:46 +0800

    fix:点击头部下拉框后不返回最初页面 update:html中增加scipt上报高度

--- src/app/cs-comp/cs-header/cs-header.component.ts
+++ src/app/cs-comp/cs-header/cs-header.component.ts
@@ -2,7 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { NzGridModule } from 'ng-zorro-antd/grid'
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'cs-header',
   standalone: true,
@@ -12,14 +12,31 @@ import { Router } from '@angular/router';
 })
 export class CsHeaderComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
 
   ngOnInit(): void {
     
   }
 
-  goToPage(page:string){
-    this.router.navigate([page]);
+  goToPage(subPath:string){
+    // this.router.navigate([subPath]);
+    
+    // 如果路径与当前路径相同，刷新当前页面
+    const currentUrl = this.router.url;
+    const path = subPath;
+    if (currentUrl.endsWith(path)) {
+      // 当前是相同路径，先导航到一个中间页面（如空路由），再跳回来
+      this.router.navigateByUrl('/refresh', { skipLocationChange: true }).then(() => {
+        this.router.navigate([path], { relativeTo: this.route });
+      });
+    } else {
+      this.router.navigate([path], { relativeTo: this.route });
+    }
+  
+    console.log(subPath, 'handleMenuClick');
   }
 
 }
